bd68493381615258bfa27bc180f211e39825a512
Nurudeen Oyeleye <oyeleyenurudeen@yahoo.com>
2022-04-07 16:20:03 +0100

    New commit
    Single Console.log

--- array.js
+++ array.js
@@ -1,13 +1,9 @@
 //First Question
 var array=["Banana", "Apples", "Oranges", "Blueberries"];
 array.shift();
-console.log(array);
 array.sort();
-console.log(array);
 array.push("Kiwi");
-console.log(array);
 array.shift();
-console.log(array);
 array.reverse();
 console.log(array);
 
