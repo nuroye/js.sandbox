18c233b5c0070075d8864ccba0c5da3d48c6a782
Nurudeen Oyeleye <oyeleyenurudeen@yahoo.com>
2022-04-07 10:47:17 +0100

    New Commit
    Fruits Array

---
+++ array.js
@@ -0,0 +1,17 @@
+//First Question
+var array=["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift();
+console.log(array);
+array.sort();
+console.log(array);
+array.push("Kiwi");
+console.log(array);
+array.shift();
+console.log(array);
+array.reverse();
+console.log(array);
+
+
+//Second Question
+var array2=["Banana",["Oranges"], "Blueberries"];
+console.log(array2[1] [0]);
