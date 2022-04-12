205a7307fa89771feacea5e1b144c89e44956ebd
Nurudeen Oyeleye <oyeleyenurudeen@yahoo.com>
2022-04-12 18:28:40 +0100

    New commit
    Add even numbers

---
+++ even.js
@@ -0,0 +1,18 @@
+
+sum=0;
+function evenNumber(sum){
+    return  sum=sum+i;
+ }
+ num=0;
+ num=prompt("Enter the nth value");
+for(i=0; i<=num; i++) {
+    if(i % 2===0){
+        sum=sum+i;
+        
+        
+       
+    }
+   
+}
+  
+  
