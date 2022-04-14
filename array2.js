9dfc48b5e8312babed73e6e13846708c90f53aca
Nurudeen Oyeleye <oyeleyenurudeen@yahoo.com>
2022-04-14 21:54:25 +0100

    New commit

--- array2.js
+++ array2.js
@@ -1,66 +1,17 @@
 /* 1. Create an object that has properties
  "username" and "password". Fill those values in with strings.
 */
-var nuf={
-    username: "nuroye",
-    password: "nuftech2022"
+var user={
+    username: "nuroye", 
+    password: "nuftech2022", 
+    status: "active" 
 }
-console.log(nuf);
 
 
 /* 2. Create an array which contains the object you have made 
 above and name the array "database".*/
 
-var database=[
-    {
-        username: "nuroye",
-        password: "nuftech2022",
-        status:"active"
-    }
-]
-
-database.push(login)
-
-/*3. Accept username, password & status from the prompt 
-and add it to the database array */
-
-var login=
-{
- prompt("Please Enter Username");
- prompt("Please Enter Password")
-prompt("Please Enter Status")
-}
-database.push(login)
-
-
-function signin(username,password){
-if (cool1 === database[0].username && cool2 === database[0].password){
-    console.log("You're Welcome");
-}
-    else  {
-        console.log("Invalid Input... Please try again");
-
-    }
-
-return (nuroye,nuftech2022);
-}
-    
-    
-
-
-
-
- function signin(username,password){
-    if (signin(database.username) ==="nuroye" && signin(database.password) ==="nuftech2022"){
-        console.log("You're Welcome")
-    }
-    else{
-    return(nuroye,nuftech2022);
-}
-}
-
-
-database[
+var database =[
     {
         username: "nuroye", 
         password: "nuftech2022", 
@@ -86,11 +37,51 @@ database[
         username: "akin", 
         password: "akin2022", 
         status: "confirmed"
+    },
+
+
+    {
+        username: "nuroye",
+        password: "nuftech2022",
+        status:"active"
     }
-      
+
 ]
-function signin(){
-    for(i=0; i < database.length; i++){
+
+database.push(login)
+
+/*3. Accept username, password & status from the prompt 
+and add it to the database array */
+
+var userName=prompt("Please Enter Username");
+var password=  prompt("Please Enter Password")
+var status=prompt("Please Enter Status")
+
+
+database.push({
+    username: userName,
+    password:password,
+    status:status
+})
+
+
+function signinForEach(username, password){
+    database.forEach(function(user){
+        console.log(user)
+        if (user.password === password && user.usename === username){
+        console.log("Your username is" + user.username + "Your password is" + user.password );
+            return
+        }
+        else{
+            console.log("Sorry, wrong username and password");
+        
+        }
+    })
+}
+
+
+function signInForLoop(username, password){
+    for(var i=0; i < database.length; i++){
         if(username === database[i].username && 
             password === database[i].password)
            {
@@ -99,3 +90,5 @@ function signin(){
     }
 
 }
+
+signinForEach("nuroye, nuftech2022");
